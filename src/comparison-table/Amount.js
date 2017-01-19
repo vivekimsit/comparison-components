0b9460804e8b97b5ba1dbade9ac97d4fdf31987e
Kevin Delemme <kevin.delemme@transferwise.com>
2017-01-19 12:11:28 +0000

    Fix props warning

--- src/comparison-table/Amount.js
+++ src/comparison-table/Amount.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 export default class Amount extends Component {
   render() {
-    const {currency, value, ...props} = this.props;
+    const {currency, value, precision, ...props} = this.props;
     return (
-      <strong {...props}>{parseFloat(value).toFixed(this.props.precision || 2)} <span className="text-primary">{currency}</span></strong>
+      <strong {...props}>{parseFloat(value).toFixed(precision || 2)} <span className="text-primary">{currency}</span></strong>
     );
   }
 }
